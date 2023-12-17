4191165d54c96ac7f9568e86c2cf83fce0f07a42
Víctor Barrios <sygmaz19@gmail.com>
2023-12-17 23:24:15 +0100

    Add EditButton to the navigation bar

--- RemovingRows/ContentView.swift
+++ RemovingRows/ContentView.swift
@@ -5,25 +5,30 @@ struct ContentView: View {
     @State private var currentNumber = 1
     
     var body: some View {
-        VStack {
-            
-            /// Here’s our first quirk:
-            ///     The onDelete() modifier only exists on ForEach,
-            ///     so if we want users to delete items from a list
-            ///     we must put the items inside a ForEach.
-            
-            List {
-                ForEach(numbers, id: \.self) {
-                    Text("Row \($0)")
+        NavigationStack {
+            VStack {
+                
+                /// Here’s our first quirk:
+                ///     The onDelete() modifier only exists on ForEach,
+                ///     so if we want users to delete items from a list
+                ///     we must put the items inside a ForEach.
+                
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete { indexSet in
+                        numbers.remove(atOffsets: indexSet)
+                    }
                 }
-                .onDelete { indexSet in
-                    numbers.remove(atOffsets: indexSet)
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
                 }
             }
-            
-            Button("Add Number") {
-                numbers.append(currentNumber)
-                currentNumber += 1
+            .toolbar {
+                EditButton()
             }
         }
     }
